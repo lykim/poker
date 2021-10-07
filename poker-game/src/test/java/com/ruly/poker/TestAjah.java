aabfde12a96460163d41b996c0d830b62a513f7a
ruly <lykim176@gmail.com>
2021-10-07 12:13:18 +0700

    set library to default use java 8 and add depedency of junit5

---
+++ poker-game/src/test/java/com/ruly/poker/TestAjah.java
@@ -0,0 +1,20 @@
+package com.ruly.poker;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestAjah {
+	
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testFalse() {
+		assertFalse(false);
+	}
+	
+}
